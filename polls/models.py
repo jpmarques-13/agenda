07dbb8e62cf824418abeeb3c4a1c92cb2e75b72a
jpmarques-13 <jp.marques@lais.huol.ufrn.br>
2019-08-13 15:40:36 -0300

    custom validator para Email

--- polls/models.py
+++ polls/models.py
@@ -4,12 +4,13 @@ from datetime import datetime
 from django.db import models
 from django.utils.timesince import timesince
 from django.utils import timezone
+from . validators import validate_school_email
 
 # Create your models here.
 
 class Contato(models.Model):
     Nome = models.CharField(max_length=250)
-    Email = models.EmailField()
+    Email = models.EmailField(null=True,validators=[validate_school_email])
     Celular =  models.CharField(max_length=14)
     Data = models.DateField(blank=True)
     Age = models.IntegerField(default=20)
